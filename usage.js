3db27a1995c33b39f588cb24bda211e6489b0031
Reza Andriansyah <reza@Reza>
2021-03-08 05:39:12 +0700

    change to buffer option

--- usage.js
+++ usage.js
@@ -2,7 +2,7 @@ const Mongo = require('./src/mongodb/init');
 
 async function main(){
   const user = await (new Mongo()).setCollection("todo").setDatabase("kotakjualan").build();  
-  console.log(await user.find());
+  console.log(Buffer.from(JSON.stringify(await user.find()), "utf8"));
 }
 
 main();
